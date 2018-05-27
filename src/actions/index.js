210ebd8a489f35203681126f1f0550bcfc5bcdf5
mladenstojanovic <mladja92@yahoo.com>
2018-05-27 09:30:54 +0200

    Added stuff

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,25 @@
+import axios from 'axios';
+
 export const EXPAND_SUGGESTION = 'EXPAND_SUGGESTION';
+export const GET_DATA = 'GET_DATA';
+
+const baseUrl = 'http://localhost:3030';
 
 export function expandSuggestion(suggestionType) {
     return (dispatch) => {
         dispatch({ type: EXPAND_SUGGESTION, payload: suggestionType });
     };
+}
+
+export function getData() {
+    console.log('zvao se get data');
+    return (dispatch) => {
+        axios.post(baseUrl + '/social', {
+            keywords: 'trump',
+        }).then((res) => {
+            dispatch({ type: GET_DATA, payload: res })
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
 }
